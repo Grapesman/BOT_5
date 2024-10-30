b2ced57cb5c320907879c5dd016a6a58799d232f
igorvinogradov <vinogradovigor2003@gmail.com>
2024-10-30 21:48:28 +0300

    Изменение бота от 30.10.2024

--- BOT_5/Graph.py
+++ BOT_5/Graph.py
@@ -1,19 +1,22 @@
-import pandas as pd
 import matplotlib.pyplot as plt
-import datetime
-def make_graph(check_state_in_dict, date_check_make_in_dict, date_state_3m):
+import pandas as pd
+import datetime, io
+
+def graf(check_state_in_dict, date_check_make_in_dict, date_state_3m):
     # Создаем датафрейм для построения графика
+    global buf
     data = {
         'Категория': check_state_in_dict,
         'Начальная дата': date_check_make_in_dict,
         'Конечная дата': date_state_3m,
     }
+
     # Конвертируем данные в DataFrame
     df = pd.DataFrame(data)
 
     # Рассчитываем длину каждого столбца диаграммы
     df['Длительность'] = pd.to_datetime(df['Конечная дата']) - pd.to_datetime(df['Начальная дата'])
-
+    plt.ion()
     # Формируем график
     fig, ax = plt.subplots()
     # Снова определяем текущую дату для построения на диаграмме сегодняшней линии
@@ -31,7 +34,9 @@ def make_graph(check_state_in_dict, date_check_make_in_dict, date_state_3m):
     for i in range(len(df)):
         start_date = df['Начальная дата'][i]
         duration_days = df['Длительность'][i].days
-        ax.barh(y_positions[i], duration_days, left=(start_date - start_date_for_diagr.date()).days, align='center', color='lightblue')
+        ax.barh(y_positions[i], duration_days, left=(start_date - start_date_for_diagr.date()).days,
+                align='center',
+                color='lightblue')
 
     # Добавление линии сегодняшней даты
     today_line = datetime.date.today()
@@ -43,12 +48,13 @@ def make_graph(check_state_in_dict, date_check_make_in_dict, date_state_3m):
         if isinstance(text, str):  # Проверяем, является ли элемент строкой
             return text[:50]  # Обрезаем до 50 символов
         return text  # Если это не строка, возвращаем как есть
+
     # Применяем функцию к элементу 'Категория' в DataFrame
-    dtxt = df['Категория'].apply(truncate)
+    dfack = df['Категория'].apply(truncate)
 
     # Настройка осей
     ax.set_yticks(y_positions)
-    ax.set_yticklabels(dtxt, fontsize=6)
+    ax.set_yticklabels(dfack, fontsize=6)
     plt.title('ДК написания статей', pad=10, x=-0.4, y=1.0, fontsize=12)
 
     # Настраиваем ось X для отображения дат
@@ -59,4 +65,8 @@ def make_graph(check_state_in_dict, date_check_make_in_dict, date_state_3m):
     plt.legend(loc='lower left', fontsize=6)
     plt.grid()
     plt.tight_layout()
-    plt.savefig('plot.jpg')
+    # Сохранение графика в памяти (в буфер)
+    buf = io.BytesIO()
+    plt.savefig(buf, format='jpg')
+    buf.seek(0)  # Перемещаемся в начало буфера
+    plt.close()  # Закрываем фигуру, чтобы избежать отображения графика
--- BOT_5/main.py
+++ BOT_5/main.py
@@ -1,80 +1,43 @@
-import os
+# Импортируем библиотеки
+import os, telebot, time, threading
 from dotenv import load_dotenv
 load_dotenv()
-import Yandex_disk, Functions, Graph, Telegram
-#Вызываем файл с функцией загрузки Excel файла с Яндекс-диска
-Yandex_disk.download_file_from_yandex_disk(os.getenv('TOKEN'), os.getenv('DIRECTORY'), os.getenv('SAVE_PATH'))
-# ----------------------------------------------------------------------------------------------------------------------
-# ------------------------------------------ВЫПОЛНЕНИЕ ФУНКЦИЙ ДЛЯ 1 КОМАНДЫ--------------------------------------------
-# Считаем количество строк в файле по листу "Каталог статей"
-stroki = Functions.how_much_string(os.getenv('SAVE_PATH'))
-# Считаем количество строк в файле по листу "Community"
-stroki_community = Functions.how_much_string_community(os.getenv('SAVE_PATH'))
-# Формируем словарь с фамилиями и тегами с листа "Community"
-data_dict = Functions.make_dict(os.getenv('SAVE_PATH'))
+import Yandex_disk, Function1, Function2, Graph
+# --------------------------------------------------КОД ТГ БОТА---------------------------------------------------------
+# Основной код
+bot = telebot.TeleBot(os.getenv('TOKEN_bot'))
+# Функция, обрабатывающая команду старт
+@bot.message_handler(commands=["status"])
+def start(m, res=False):
+    if all_teg:
+        bot.send_message(m.chat.id, "<b>Следующим авторам необходимо заполнить Таблицу статей:</b>" + "\n" + '\n'.join(all_teg), parse_mode = 'HTML')
+    bot.send_message(m.chat.id, "<b>Авторам данных статей необходимо заполнить Таблицу статей:</b>" + "\n - " + '\n - '.join(names_state), parse_mode = 'HTML')
 
-# Проверяем поочередно условия для листа "Каталог статей"
-# Проверка первого условия
-FIOs_without_name = Functions.f_FIOs_without_name(os.getenv('SAVE_PATH'))
-Fs_without_name = Functions.f_Fs_without_name(FIOs_without_name)
-Familys_without_name = Functions.f_Familys_without_name(Fs_without_name)
+@bot.message_handler(commands=["notes"])
+def send_photo_file(message):
+    chat_id = message.chat.id
+    try:
+        bot.send_photo(chat_id=chat_id, photo=buf)
+        buf.close()
+    except Exception as e:
+        print(f"Ошибка при отправке фотографии: {e}")
+#
+#------------------------------------------------------------------------------------------------------------------------
 
-# Проверка второго условия
-FIOs_without_date = Functions.f_FIOs_without_date(os.getenv('SAVE_PATH'))
-Fs_without_date = Functions.f_Fs_without_date(FIOs_without_date)
-Familys_without_date = Functions.f_Familys_without_date(Fs_without_date)
+def start_bot():
+    bot.polling(none_stop=True, interval=0)
 
-# Проверка третьего условия
-FIOs_without_thesis = Functions.f_FIOs_without_thesis(os.getenv('SAVE_PATH'))
-Fs_without_thesis = Functions.f_Fs_without_thesis(FIOs_without_thesis)
-Familys_without_thesis = Functions.f_Familys_without_thesis(Fs_without_thesis)
-
-# Проверка четвертого условия
-FIOs_without_key = Functions.f_FIOs_without_key(os.getenv('SAVE_PATH'))
-Fs_without_key = Functions.f_Fs_without_key(FIOs_without_key)
-Familys_without_key = Functions.f_Familys_without_key(Fs_without_key)
-
-# Проверка пятого условия
-# Находим статьи, с заполненным названием, но без автора
-names_state = Functions.f_state_name(os.getenv('SAVE_PATH')) #<--------- это выводим в бот в команде ["status"]
-# Объединяем все полученные списки (1-4 условий) с фамилиями в один список
-result = Functions.f_result_Familys (Familys_without_name, Familys_without_date, Familys_without_thesis, Familys_without_key)
-# Исключаем повторы фамилий в списке
-without_duplication = Functions.remove_duplicates(result)
-# По получившимся фамилиям находим теги из словаря
-all_teg = Functions.teg_function(without_duplication) #<--------- это выводим в бот в команде ["status"]
-# ----------------------------------------------------------------------------------------------------------------------
-# ------------------------------------------ВЫПОЛНЕНИЕ ФУНКЦИЙ ДЛЯ 2 КОМАНДЫ--------------------------------------------
-# Сохраняем список дат публикации статей
-DATA = Functions.date_check(os.getenv('SAVE_PATH'))
-# Сохраняем названия статей
-STATE = Functions.name_check(os.getenv('SAVE_PATH'))
-# Сохраняем список дат создания статей
-MAKE = Functions.make_check(os.getenv('SAVE_PATH'))
-
-# Создаем словарь DATA & STATE
-DICT_DATA_STATE = Functions.function_DICT_DATA_STATE(DATA, STATE)
-# Создаем словарь STATE & MAKE
-DICT_STATE_MAKE = Functions.function_DICT_STATE_MAKE(STATE, MAKE)
-
-# Определяем даты публикации, относительно текущей, в диапазоне до 180 дней и сохраняем в виде списка
-checking = Functions.compare_dates()
-
-# Убираем повторяющиеся даты из списка
-state_3m = Functions.remove_duplicates_3m(checking)
-
-# Сохраняем список статей по данным датам
-check_state_in_dict = Functions.state_function(state_3m, DICT_DATA_STATE)
-
-# Находим даты создания статьи по её названию
-check_make_in_dict = Functions.make_function(check_state_in_dict, DICT_STATE_MAKE)
-
-# Переводим списки дат создания и публикации из формата date в формат datetime
-date_check_make_in_dict = [dt.date() for dt in check_make_in_dict]
-date_state_3m = [dt.date() for dt in state_3m]
-
-# Вызываем файл Graph и создаем график
-Graph.make_graph(check_state_in_dict, date_check_make_in_dict, date_state_3m)
-# Вызываем файл Telegram и запускаем бота
-Telegram.bot(os.getenv('TOKEN_bot'), all_teg, names_state)
-# ----------------------------------------------------------------------------------------------------------------------
+# Запускаем бота в отдельном потоке
+thread = threading.Thread(target=start_bot)
+thread.start()
+# Запускаем второй поток для парсинга
+while True:
+    Yandex_disk.download_file_from_yandex_disk(os.getenv('TOKEN'), os.getenv('DIRECTORY'), os.getenv('SAVE_PATH'))
+    Function1.function1()
+    from Function1 import all_teg, names_state
+    Function2.function2()
+    from Function2 import check_state_in_dict, date_check_make_in_dict, date_state_3m
+    Graph.graf(check_state_in_dict, date_check_make_in_dict, date_state_3m)
+    from Graph import buf
+    os.remove('Table_Таблица статей.xlsx')
+    time.sleep(1)  # Добавьте небольшую задержку, чтобы избежать перегрузки процессора
